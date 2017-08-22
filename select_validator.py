687742a0e8458b2941ad9c49dd502143dedd4a7d
simran singhal <singhalsimran0@gmail.com>
2017-08-22 10:28:39 +0530

    Added from, where and select validator

--- select_validator.py
+++ select_validator.py
@@ -1,3 +1,32 @@
+import re
 
 #Select validator
 def SelectValidation(query):
+    temp_query = query[0].strip().split(" ",1) #Only 1st space is split
+    query = re.findall(r'[^,\s]+', query[0])  #split about both space and ','
+
+    #check first element of list is 'SELECT' or not
+    if query[0] != 'SELECT':
+        return False, ''
+
+    #Check if any column is present in query
+    if len(query) <= 1:
+        return False, ''
+
+    for i in range(len(query)):
+        #Detecting queries like *col1 (forgot ',' in between * and col1)
+        if '*' in query[i] and len(query[i]) > 1:
+            return False, ''
+
+        #'*' can not be present at any other place like "Select col1, * from Table1"
+        if '*' in query[i] and i != 1:
+            return False, ''
+
+    temp_query = temp_query[1].strip().split(",");  #Splitting column names about ','
+
+    #Detecting case like 'Select abc, pr, from' or 'Select *, from'
+    if '' in temp_query:
+        return False, ''
+
+    query.pop(0)
+    return True, query
