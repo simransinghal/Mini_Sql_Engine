ff2e2de09efc471e370bab6f8a32a8cd92cbd4d2
simran singhal <singhalsimran0@gmail.com>
2017-08-24 01:29:28 +0530

    Add files

--- execute_where.py
+++ execute_where.py
@@ -14,9 +14,11 @@ def execute_where(joinTable, where_query, from_query, require_tables):
     if len(conditions) == 1:
         result, table = SingleCondition(conditions, attr_dictionary, ops_dictionary, joinTable)
         if result == False:
-            return False, table
+            return False, []
 
     else:
         result, table = DoubleCondition(conditions, attr_dictionary, ops_dictionary, joinTable, operator)
         if result == False:
-            return False, table        
+            return False, []
+
+    return True, table
